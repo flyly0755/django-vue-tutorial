2a7ebc8c4c032a28b9764f2500322d55de66cf85
colinli <flyly0755@163.com>
2022-04-13 21:30:52 +0800

    add package django_debug_toolbar for debug

--- drf_vue_blog/settings.py
+++ drf_vue_blog/settings.py
@@ -58,6 +58,8 @@ INSTALLED_APPS = [
     'django_admin_hstore_widget',
     'django_json_widget',
     'django.contrib.postgres',
+    'django_better_admin_arrayfield',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +70,25 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+# 添加访问IP
+INTERNAL_IPS = ('127.0.0.1',)
+
 
 ROOT_URLCONF = 'drf_vue_blog.urls'
 
